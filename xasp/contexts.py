3d6d0835835de1426e2ff9b9178367d9af11d3e4
Mario Alviano <mario.alviano@gmail.com>
2022-11-29 13:55:14 +0100

    refactor well founded model

--- xasp/contexts.py
+++ xasp/contexts.py
@@ -1,4 +1,5 @@
 import dataclasses
+from functools import lru_cache, cached_property
 from typing import Optional
 
 from clingo import Number, String
@@ -27,7 +28,7 @@ class ProcessAggregatesContext:
         assert False
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class ComputeMinimalAssumptionSetContext:
     Rule = namedtuple("Rule", "id head pos_body neg_body")
     WellFoundedModel = namedtuple("WellFoundedModel", "true potentially_true")
@@ -36,8 +37,6 @@ class ComputeMinimalAssumptionSetContext:
     atom2heads: dict = dataclasses.field(default_factory=lambda: defaultdict(list))
     atom2pos_bodies: dict = dataclasses.field(default_factory=lambda: defaultdict(list))
     atom2neg_bodies: dict = dataclasses.field(default_factory=lambda: defaultdict(list))
-
-    well_founded_model: Optional = dataclasses.field(default=None)
 
     @staticmethod
     def output(atom, explanation):
@@ -70,69 +69,71 @@ class ComputeMinimalAssumptionSetContext:
         return Number(1)
 
     def false_in_well_founded_model(self, atom):
-        def enqueue_if_source_pointer(rule, my_queue):
-            if rule.head is None:
-                return
-            if all(source_pointer[head_atom] is not None for head_atom in rule.head):
-                return
-            log.debug(f"check {rule.id}")
-            if any(batom not in self.well_founded_model.potentially_true for batom in rule.pos_body):
-                return
-            log.debug(f"  pos_body potentially true")
-            if any(batom in self.well_founded_model.true for batom in rule.neg_body):
-                return
-            log.debug(f"  neg_body potentially true")
-            if all(source_pointer[batom] is not None for batom in rule.pos_body):
-                for head_atom in rule.head:
-                    if source_pointer[head_atom] is None:
-                        log.debug(f"source pointer: {head_atom} {rule.id}")
-                        source_pointer[head_atom] = rule
-                        my_queue.append(head_atom)
-
-        if self.well_founded_model is None:
-            self.well_founded_model = self.WellFoundedModel(
-                set(),
-                set().union(*(rule.head for rule in self.rules.values() if rule.head))
-            )
-            source_pointer = {ptatom: None for ptatom in self.well_founded_model.potentially_true}
-            source_pointer_of_atom = {rule: None for rule in self.rules}
-
-            while True:
-                queue = list(set().union(*(rule.head for rule in self.rules.values() if
-                                           len(rule.pos_body) + len(rule.neg_body) == 0 and rule.head)))
-                while queue:
-                    true_atom = queue[-1]
-                    queue.pop()
-
-                    if true_atom in self.well_founded_model.true:
-                        continue
-                    assert true_atom in self.well_founded_model.potentially_true
-
-                    self.well_founded_model.true.add(true_atom)
-                    log.debug(f"true: {true_atom}")
-                    for rule in self.atom2pos_bodies[true_atom]:
-                        if all(batom in self.well_founded_model.true for batom in rule.pos_body) and \
-                                all(batom not in self.well_founded_model.potentially_true for batom in rule.neg_body):
-                            for head_atom in rule.head:
-                                queue.append(head_atom)
-
-                queue = []
-                for rule in self.rules.values():
-                    enqueue_if_source_pointer(rule, queue)
-                while queue:
-                    atom_with_source_pointer = queue[-1]
-                    queue.pop()
-
-                    for rule in self.atom2pos_bodies[atom_with_source_pointer]:
-                        enqueue_if_source_pointer(rule, queue)
-                founded_atoms = set(spatom for spatom in source_pointer if source_pointer[spatom] is not None)
-                unfounded_atoms = self.well_founded_model.potentially_true - founded_atoms
-                if not unfounded_atoms:
-                    break
-
-                for unfounded_atom in unfounded_atoms:
-                    log.debug(f"false: {unfounded_atom}")
-                    assert unfounded_atom not in self.well_founded_model.true
-                    self.well_founded_model.potentially_true.remove(unfounded_atom)
-
         return Number(1) if atom not in self.well_founded_model.potentially_true else Number(0)
+
+    @cached_property
+    def well_founded_model(self) -> 'ComputeMinimalAssumptionSetContext.WellFoundedModel':
+        well_founded_model = self.WellFoundedModel(
+            set(),
+            set().union(*(rule.head for rule in self.rules.values() if rule.head))
+        )
+        source_pointer = {atom: None for atom in well_founded_model.potentially_true}
+
+        while True:
+            queue = list(set().union(*(rule.head for rule in self.rules.values() if
+                                       len(rule.pos_body) + len(rule.neg_body) == 0 and rule.head)))
+            while queue:
+                true_atom = queue[-1]
+                queue.pop()
+
+                if true_atom in well_founded_model.true:
+                    continue
+                assert true_atom in well_founded_model.potentially_true
+
+                well_founded_model.true.add(true_atom)
+                log.debug(f"true: {true_atom}")
+                for rule in self.atom2pos_bodies[true_atom]:
+                    if all(atom in well_founded_model.true for atom in rule.pos_body) and \
+                            all(atom not in well_founded_model.potentially_true for atom in rule.neg_body):
+                        for head_atom in rule.head:
+                            queue.append(head_atom)
+
+            queue = []
+            for rule in self.rules.values():
+                self.enqueue_if_source_pointer(rule, queue, source_pointer, well_founded_model)
+            while queue:
+                atom_with_source_pointer = queue[-1]
+                queue.pop()
+
+                for rule in self.atom2pos_bodies[atom_with_source_pointer]:
+                    self.enqueue_if_source_pointer(rule, queue, source_pointer, well_founded_model)
+            founded_atoms = set(atom for atom in source_pointer if source_pointer[atom] is not None)
+            unfounded_atoms = well_founded_model.potentially_true - founded_atoms
+            if not unfounded_atoms:
+                break
+
+            for unfounded_atom in unfounded_atoms:
+                log.debug(f"false: {unfounded_atom}")
+                assert unfounded_atom not in well_founded_model.true
+                well_founded_model.potentially_true.remove(unfounded_atom)
+        return well_founded_model
+
+    @staticmethod
+    def enqueue_if_source_pointer(rule, my_queue, source_pointer, well_founded_model):
+        if rule.head is None:
+            return
+        if all(source_pointer[head_atom] is not None for head_atom in rule.head):
+            return
+        log.debug(f"check {rule.id}")
+        if any(atom not in well_founded_model.potentially_true for atom in rule.pos_body):
+            return
+        log.debug(f"  pos_body potentially true")
+        if any(atom in well_founded_model.true for atom in rule.neg_body):
+            return
+        log.debug(f"  neg_body potentially true")
+        if all(source_pointer[atom] is not None for atom in rule.pos_body):
+            for head_atom in rule.head:
+                if source_pointer[head_atom] is None:
+                    log.debug(f"source pointer: {head_atom} {rule.id}")
+                    source_pointer[head_atom] = rule
+                    my_queue.append(head_atom)
